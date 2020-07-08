0e425e01c14f95aeff30309eb213e9f3668f74d7
Frank Fan <frankfancode@gmail.com>
2020-07-08 22:15:59 +0800

    first commit

---
+++ doc/precache-manifest.e87167721f66b3c06e454a15e160107f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74f42c1eb4a4dca588d7625455af1a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ef69da1ae236a50000",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ef69da1ae236a50000",
+    "url": "/static/js/main.5664a3a7.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
